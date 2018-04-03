cc87e4ff52af08ea8183bdd521b22fcfbd4dfad1
Olivier P <provostolivier@hotmail.com>
2018-04-03 11:35:57 -0400

    Small grid + Camera control
    Added a small grid to the main game and a Camera controller so you can now move the camera with WASD or the arrows and you can reset it to its default place with the spacebar.

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    Transform tf;
+    float newX, newY;
+
+	// Use this for initialization
+	void Start () {
+        tf = GetComponent<Transform>();
+        newX = tf.position.x;
+        newY = tf.position.y;
+	}
+
+    void Update()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+        move.y = Input.GetAxis("Vertical");
+
+        newX += move.x;
+        newY += move.y;
+
+        if(Input.GetButton("ResetCamera"))
+        {
+            Debug.Log("Space pressed");
+            newX = 0;
+            newY = 0;
+        }
+
+        tf.position = new Vector3(newX, newY, -10);
+    }
+}
