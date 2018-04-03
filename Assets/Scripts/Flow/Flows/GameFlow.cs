6f92133c780e27381503d3896e0f1af6ceb5df82
Olivier P <provostolivier@hotmail.com>
2018-04-03 08:46:05 -0400

    Flow scripts
    Creation of the Unity project
    Added the scripts for the gameflows (MainEntryCreator, MainEntry, Flow, GameFlow, Flowmanager and MainMenuFlow)

---
+++ Assets/Scripts/Flow/Flows/GameFlow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFlow : Flow {
+
+    public override void InitializeFlow()
+    {
+        
+    }
+
+    public override void UpdateFlow(float dt)
+    {
+        
+    }
+
+    public override void FixedUpdateFlow(float dt)
+    {
+        
+    }
+}
