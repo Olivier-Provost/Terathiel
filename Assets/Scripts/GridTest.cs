6ffc3b413d66b44667152948b6672e356b770fb9
Olivier P <provostolivier@hotmail.com>
2018-04-04 11:49:39 -0400

    New Grid test
    I added a new scene to test a new way to make the grid.

---
+++ Assets/Scripts/GridTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTest {
+
+    readonly float TILES_WIDTH = 10;
+
+    public int nbTiles;
+    
+	
+}
