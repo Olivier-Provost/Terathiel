c58cb23f225a08fd6d3868d12d69bff64913b091
f0rzero <sam_hockey_34@hotmail.ca>
2018-04-04 01:17:29 -0400

    Health bar and player Picture
    UI wher the player can see the other player life . i put a little script to check if the health bar was working

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour {
+
+    public float max_health = 100f;
+    public float current_Health = 0f;
+    public GameObject healthbar;
+    public float calc_health;
+
+	// Use this for initialization
+	void Start () {
+        current_Health = max_health;
+        InvokeRepeating("DecreasedHealth", 1f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void DecreasedHealth()
+    {
+        current_Health -= 2;
+        calc_health = current_Health / max_health;
+        SetHealthBar(calc_health);
+    }
+    public void SetHealthBar(float myHealth)
+    {
+        healthbar.transform.localScale = new Vector3(healthbar.transform.localScale.x, myHealth, healthbar.transform.localScale.z);
+    }
+}
+
