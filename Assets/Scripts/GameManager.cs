e17b9feff0fb59bf3fa2a3c8d93d7c505e7983bd
f0rzero <sam_hockey_34@hotmail.ca>
2018-04-09 23:41:28 -0400

    Création d'un petit gameManager et d'un PlayerManager
    création du manager de player et de game pour aider le flow du jeux

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager  {
+
+    #region singleton
+    private static GameManager instance;
+
+    private GameManager() { }
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new GameManager();
+
+            return instance;
+        }
+    }
+    #endregion singleton
+
+    private float turnCount = 1;
+    private float playerWhoPlay = 1;
+    private bool isGameEnded = false;
+    private transform player1, player2;
+
+    public void Init(Transform _player1, Transform _player2)
+    {
+        player1 = _player1;
+        player2 = _player2;
+
+        player1.GetComponent<>().Init();
+        player2.GetComponent<>().Init();
+    }
+
+    public float GetTurnCount()
+    {
+        return turnCount;
+    }
+
+    public float GetCurrentPlayer()
+    {
+        return playerWhoPlay;
+    }
+
+    public void NextTurn()
+    {
+        
+
+        if (playerWhoPlay >= GV.NUMBER_OF_PLAYER)
+        {
+            playerPlaying = 1;
+            turnCount += 1;
+        }
+        else
+            playerPlaying += 1;
+
+        //InputManager.Instance.NextTurn();
+        PlayerManager.Instance.NextTurn(turnCount);
+      
+        UIManager.Instance.NextTurn(turnCount, playerWhoPlay);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager  {
+
+    #region singleton
+    private static PlayerManager instance;
+
+    private PlayerManager() { }
+
+    public static PlayerManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new PlayerManager();
+
+            return instance;
+        }
+    }
+    #endregion singleton
+
+    private Dictionary<float, Player> players = new Dictionary<float, Player>();
+
+    public void Init()
+    {
+        Player player_1 = new Player();
+        Player player_2 = new Player();
+
+        players.Add(1f, player_1);
+        players.Add(2f, player_2);
+    }
+    public void NextTurn(float _turn)
+    {
+        if (_turn > 1)
+            players[GameManager.Instance.GetCurrentPlayer()].UpdateCoins(GV.NEW_TURN_COINS);
+    }
+    public void upDateAP(float cost)
+    {
+        players[GameManager.Instance.GetCurrentPlayer()].upDateAP(cost);
+    }
+}
